d090f30ed12ae6e397893c696740954645bc0da5
XiaoYi <Admin@xiaoyi.fit>
2023-12-11 07:47:49 +0800

    update: 1. handle no param

--- parameter.ps1
+++ parameter.ps1
@@ -1,5 +1,10 @@
 $argCount = $args.Count
 
+if ($argCount -eq 0) {
+    Write-Host "No arguments"
+    exit
+}
+
 for ($i = 0; $i -le $argCount - 1; $i++) {
     $arg = $args[$i]
     Write-Host "Argument[$i]: $arg"
